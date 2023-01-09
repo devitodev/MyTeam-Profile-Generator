f8790a7337849c82fede57808294e28b13b0c775
devitodev <softwaredev.ito@gmail.com>
2023-01-08 21:00:57 -0600

    Added all of the code

--- index.js
+++ index.js
@@ -1,31 +1,126 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee');
+const path = require('path')
+
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const managerArray = []
+const engineerArray = []
+const internArray = []
+const generateHTML = require('./src/generateHTML')
 
 
 const questions = [
     {
         type: 'input',
-        name: 'team-member-name',
-        message: 'What is this team member\'s name?',
+        name: 'managerName',
+        message: 'What is the manager\'s name?',
     },
     {
         type: 'input',
-        name: 'job-title',
-        message: 'What is this team member\'s job title?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is this team member\'s email address?',
+        name: 'ID',
+        message: 'What is the manager\'s ID?',
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is this team member\'s GitHub username?',
-    }
+        message: 'What is the manager\'s email address?',
+    },
+    
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is manager\'s office number?',
+    },
 ]
+
+inquirer.prompt(
+    questions
+).then(answers => {
+    const manager = new Manager(answers.managerName, answers.ID, answers.email, answers.officeNumber)
+    managerArray.push(manager)
+    createTeam()
+}) 
+
+function createTeam() {
+    inquirer.prompt([{
+        type:'list',
+        name: 'choice',
+        message: 'Which type of team member is next?',
+        choices: ['Engineer', 'Intern', 'All done!']
+    }])
+    .then(answer => {
+        if (answer.choice === 'Engineer') {
+            createEngineer() 
+        } else if (answer.choice === 'Intern') {
+            createIntern()
+        } else {
+            fs.writeFileSync(path.join(__dirname,'/dist/team.html'),generateHTML(managerArray, engineerArray, internArray))
+        }
+    })
+}
+
+function createEngineer() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: 'What is the engineer\'s name?',
+            },
+            {
+                type: 'input',
+                name: 'ID',
+                message: 'What is the engineer\'s ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the engineer\'s email address?',
+            },
+            
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is engineer\'s GitHub username?',
+            },
+        ]
+    ) .then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.ID, answers.email, answers.github) 
+        engineerArray.push(engineer)
+        createTeam()
+    })
+}
+function createIntern() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'internName',
+                message: 'What is the intern\'s name?',
+            },
+            {
+                type: 'input',
+                name: 'ID',
+                message: 'What is the intern\'s ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the intern\'s email address?',
+            },
+            
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What is intern\'s school?',
+            },
+        ]
+    ) .then(answers => {
+        const intern = new Intern(answers.internName, answers.ID, answers.email, answers.school) 
+        internArray.push(intern)
+        createTeam()
+    })
+}
 
---
+++ src/generateHTML.js
@@ -0,0 +1,101 @@
+function generateHTML(managerArray, engineerArray, internArray) {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-around">
+                ${generateTeam(managerArray, engineerArray, internArray)}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `;
+
+}
+
+function generateTeam(managerArray, engineerArray, internArray) {
+    function generateManager(manager) {
+        return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+    }
+    function generateEngineer(engineer) {
+        return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item"> GitHub: ${engineer.getGitHub()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+    }
+    function generateIntern(intern) {
+        return `
+        <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item"> School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
+        `;
+    }
+    let html = ''
+    html += generateManager(managerArray[0])
+    for(let i = 0; i < engineerArray.length; i++) {
+        html += generateEngineer(engineerArray[i])
+    }
+    for(let i = 0; i < internArray.length; i++) {
+        html += generateIntern(internArray[i])
+    }
+    return html
+}
+
+module.exports = generateHTML
